8a8b84ac8f95fe425acbb52c10a3513db3282e11
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-03-28 21:40:43 -0500

    added functionability to Start quiz btn, quit and continue btns

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,96 @@
    // - StartQuizBtn, StartScreenEl, timerEl, HighscoreEl
     //- QuestionEl, questionTitleEl, AnswerChoicesEl
 
+    var startQuizBtn = document.querySelector('.startQuizBtn button');
+    var infoBox = document.querySelector('.rulesBox');
+    var quitBtn = document.querySelector('.buttons .quitQuiz');
+    var continueBtn = document.querySelector('.buttons .continue');
+    var StartQuizBtn = document.querySelector('.startQuizBtn button');
+    var quizBox = document.querySelector('.quizBox');
+
+    //If start quiz button is pressed
+    startQuizBtn.onclick = ()=>{
+       infoBox.classList.add("activeInfo"); // Shows info box
+    }
+
+    //if quit button is pressed
+    quitBtn.onclick = ()=>{
+      infoBox.classList.remove("activeInfo"); //hides the info box
+   }
+
+   //if continue button is pressed
+    continueBtn.onclick = ()=>{
+      infoBox.classList.remove("activeInfo"); //hides the info box
+      quizBox.classList.add("activeQuiz"); //Shows the quiz
+   }
+
 //Create an array of objects that hold all questions
   //  - Answer AnswerChoices 
    // - Quuestion text 
-    // - Correct answer 
+    // - Correct answer
+
+    let questions = [
+       {
+          numb: 1,
+          question: "What does CSS do?",
+          answer: "Style an HTML Page",
+          options: [
+             "Style an HTML Page",
+             "Make the website functionable",
+             "Create dynamic webpage",
+             "None of the above"
+          ]
+       },
+       {
+         numb: 2,
+         question: "What does HTML stand for?",
+         answer: "Hyper Text Preprocessor",
+         options: [
+            "Hyper Text Multiple Language",
+            "Hyper Text Markup Language",
+            "Hyper Text Preprocessor",
+            "Hyper Tool Multi Language"
+         ]
+      },
+      {
+         numb: 3,
+         question: "What data type is true/false?",
+         answer: "Boolean",
+         options: [
+            "JQuery",
+            "Boolean",
+            "String",
+            "Number"
+         ]
+      },
+      {
+         numb: 4,
+         question: "What is JQuery?",
+         answer: "Javascript Library",
+         options: [
+            "Javascript Library",
+            "An Element",
+            "DOM",
+            "A Data Type"
+         ]
+      },
+      {
+         numb: 5,
+         question: "How do I turn a String, into an Array",
+         answer: ".split() Function",
+         options: [
+            ".concat() Function",
+            ".splice() Function",
+            ".join() Function",
+            ".split() Function"
+         ]
+      },
+    ]
+
+    let questionCount = 0;
+
+
+
 
 // Create a startQuiz() function triggered by the StartQuizBtn
    // -Hide start screen
